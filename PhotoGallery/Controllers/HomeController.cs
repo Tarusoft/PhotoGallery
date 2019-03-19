aee1eabf8e186bb8bef3d181981a79a14da06103
Tarusoft <vermataruna@gmail.com>
2019-03-18 19:02:36 -0700

    Add files via upload

---
+++ PhotoGallery/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoGallery.Models;
+
+namespace PhotoGallery.Controllers
+{
+public class HomeController : Controller
+{
+    // GET: Home
+    public ActionResult Index()
+    {        
+            var photoGalleryModel = new List<PhotoGalleryModel>();
+
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "02da51eea6e8094ad64ead7e49aba578", ImagePath = "Images\\02da51eea6e8094ad64ead7e49aba578.jpg" });
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "2011_singer_porsche_911_300_1920x1080", ImagePath = "Images\\2011_singer_porsche_911_300_1920x1080.jpg" });
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "2feae2d8-a612-4ea7-b7d7-4fe5af249b69", ImagePath = "Images\\2feae2d8-a612-4ea7-b7d7-4fe5af249b69.jpg" });
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "download", ImagePath = "Images\\download.jpg" });
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "gallery-1433525772-roa070115fea-singer09b", ImagePath = "Images\\gallery-1433525772-roa070115fea-singer09b.jpg" });
+            photoGalleryModel.Add(new PhotoGalleryModel() { ImageName = "singer-porsche-911_3366036k", ImagePath = "Images\\singer-porsche-911_3366036k.jpg" });
+
+            return View(photoGalleryModel);//Json(photoGalleryModel, JsonRequestBehavior.AllowGet);
+
+        }
+}
+}
---
+++ PhotoGallery/Models/PhotoGalleryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoGallery.Models
+{
+    public class PhotoGalleryModel
+    {
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
